8f3bfc0b9029eaaa684845f3b2d43d11b018b588
Xuss-mt <M11401022@mail.ntust.edu.tw>
2026-02-01 02:07:45 +0800

    更新優惠碼 2/2 - FEBSHV02, 22CAMPOB

--- coupon-data.js
+++ coupon-data.js
@@ -1,14 +1,12 @@
 /**
  * 阿丹快省站 - 優惠碼資料
- * 此檔案由 n8n 自動更新
- * 手動測試用預設資料
+ * 此檔案由 OpenClaw 自動更新
+ * 更新時間：2026-02-02
  */
 
-// 使用 var 確保全域可用
-// eslint-disable-next-line no-unused-vars
 var COUPON_DATA = [
     {
-        "code": "GU2BP87D9H",
+        "code": "FEBSHV02",
         "threshold": 100,
         "discount": 20,
         "type": "全站券",
@@ -16,51 +14,11 @@ var COUPON_DATA = [
         "link": "https://shopee.tw/user/voucher-wallet"
     },
     {
-        "code": "MALL299M30",
-        "threshold": 299,
-        "discount": 30,
-        "type": "商城券",
-        "quantity": "有限",
-        "link": "https://shopee.tw/user/voucher-wallet"
-    },
-    {
-        "code": "MALL199M25",
-        "threshold": 199,
-        "discount": 25,
-        "type": "商城券",
-        "quantity": "無限",
-        "link": "https://shopee.tw/user/voucher-wallet"
-    },
-    {
-        "code": "FREESHIP50",
-        "threshold": 0,
-        "discount": 50,
-        "type": "免運券",
-        "quantity": "無限",
-        "link": "https://shopee.tw/user/voucher-wallet"
-    },
-    {
-        "code": "LIVE100M15",
+        "code": "22CAMPOB",
         "threshold": 100,
-        "discount": 15,
-        "type": "直播券",
-        "quantity": "有限",
-        "link": "https://shopee.tw/user/voucher-wallet"
-    },
-    {
-        "code": "XMAS500M80",
-        "threshold": 500,
-        "discount": 80,
+        "discount": 20,
         "type": "全站券",
-        "quantity": "有限",
-        "link": "https://shopee.tw/user/voucher-wallet"
-    },
-    {
-        "code": "SUPER1000M80",
-        "threshold": 1000,
-        "discount": 80,
-        "type": "全站券",
-        "quantity": "有限",
+        "quantity": "限指定用戶",
         "link": "https://shopee.tw/user/voucher-wallet"
     }
 ];
